6699a5ba86449927a56ecd8d8923e3c69df18729
joh9911 <joh9911@naver.com>
2022-07-29 10:30:14 +0900

    update

--- app/src/main/java/com/example/raise_developer/MainActivity.kt
+++ app/src/main/java/com/example/raise_developer/MainActivity.kt
@@ -3,16 +3,32 @@ package com.example.raise_developer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var personalMoney = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_page)
 
         initEvent()
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean { // 터치할 때마다 개인 자산의 TextView가 만원 씩 증가
+        when(event?.action){
+            MotionEvent.ACTION_DOWN -> {
+                personalMoney += 10000
+                findViewById<TextView>(R.id.main_page_text_view_personal_money).text = "${personalMoney}원"
+                Log.d("좌표", "${event.x}, ${event.y}")
+            }
+        }
+        return super.onTouchEvent(event)
     }
 
     fun initEvent(){
@@ -42,15 +58,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun grassShopDialogButtonEvent(){ // 잔디 상점과 연결하면 됨
-        val grassShopDialogButton = findViewById<Button>(R.id.grass_shop_dialog_button)
-        grassShopDialogButton.setOnClickListener {
-            val grassShopDialog = GrassShopDialog()
-
-            grassShopDialog.show(supportFragmentManager,"grassShopDialg")
-        }
-
-    }
+//    fun grassShopDialogButtonEvent(){ // 잔디 상점과 연결하면 됨
+//        val grassShopDialogButton = findViewById<Button>(R.id.grass_shop_dialog_button)
+//        grassShopDialogButton.setOnClickListener {
+//            val grassShopDialog = GrassShopDialog()
+//
+//            grassShopDialog.show(supportFragmentManager,"grassShopDialg")
+//        }
+//
+//    }
 
 
 
